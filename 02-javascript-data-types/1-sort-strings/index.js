c08129cb6df2ee6d1957f7a09bad719e7640a314
EFIEV <ev.efimova@severstal.com>
2024-07-28 19:33:37 +0300

    Update index.js

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -5,5 +5,12 @@
  * @returns {string[]}
  */
 export function sortStrings(arr, param = 'asc') {
-
+  let arr_f = [...arr];
+  return arr_f = Sort( (a, b) => {
+  if param === 'asc' 
+    return a.localeCompare(b,  ["ru", "en"], {caseFirst: 'upper'} );
+  if param === 'desc
+    return b.localCompare(a, ["ru", "en"], {caseFirst: 'lower'} );
+  })
 }
+
