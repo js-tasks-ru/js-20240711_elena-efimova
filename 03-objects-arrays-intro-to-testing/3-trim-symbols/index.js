7ad627ce32b991c5918f2a97b18a4703d2335a54
Elena Efimova <elenrafaelka@gmail.com>
2024-08-11 22:34:51 +0300

    2.2, 2.3, 3

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -4,6 +4,23 @@
  * @param {number} size - the allowed size of consecutive identical symbols
  * @returns {string} - the new string without extra symbols according passed size
  */
-export function trimSymbols(string, size) {
-
+export function trimSymbols(stroka, size) {
+    const res="";
+    const i = 0;
+    if (size === undefined) {
+        res == stroka;
+    }
+    else 
+    {
+    while (i < stroka.length) {
+        const current_str = stroka[i];
+        const r = i;
+        while (stroka[i] === current_str)
+            {
+                i = i++;
+            }   
+        res += current_str.repeat(i - r).slice(0, size);    
+    }
+        return res;
+}
 }
